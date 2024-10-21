69f10c17fb619c204dac06a90fbea49f7a9bc4b0
master367 <adiko7634@gmail.com>
2024-10-21 23:38:53 +0500

    1

--- src/part3/NewsAgency.java
+++ src/part3/NewsAgency.java
@@ -7,19 +7,19 @@ public class NewsAgency implements Observable{
     private List<Observer> observers = new ArrayList<>();
     private String headline;
 
-    // Add an observer to the list
+
     @Override
     public void addObserver(Observer observer) {
         observers.add(observer);
     }
 
-    // Remove an observer from the list
+
     @Override
     public void removeObserver(Observer observer) {
         observers.remove(observer);
     }
 
-    // Notify all observers of the latest headline
+
     @Override
     public void notifyObservers() {
         for (Observer observer : observers) {
@@ -27,7 +27,7 @@ public class NewsAgency implements Observable{
         }
     }
 
-    // Set a new headline and notify observers
+
     public void setHeadline(String headline) {
         this.headline = headline;
         notifyObservers();
--- src/part3/NewsChannel.java
+++ src/part3/NewsChannel.java
@@ -8,14 +8,12 @@ public class NewsChannel implements Observer {
         this.name = name;
     }
 
-    // Update method is called when the state of the observable changes
     @Override
     public void update(String headline) {
         this.currentHeadline = headline;
         display();
     }
 
-    // Display the current headline for the news channel
     public void display() {
         System.out.println(name + " broadcasting: " + currentHeadline);
     }
